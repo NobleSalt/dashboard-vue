df4f387f32b395c8ee801e282e2ea0cf33bbd2a0
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-06-25 08:52:09 +0100

    ok

--- src/components/SideRight.vue
+++ src/components/SideRight.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="SideRight">
     <div class="SideRight-head">
-      <div class="SideRight-head-left">
-        <i class="SideRight-head-left-icon">
-          <SearchIcon />
-        </i>
-        <input type="text" class="SideRight-head-left-input" placeholder="search..." />
-      </div>
+      <div class="SideRight-head-content">
+        <div class="SideRight-head-content-menu">
+          <MenuIcon />
+        </div>
+        <div class="SideRight-head-content-left">
+          <i class="SideRight-head-content-left-icon">
+            <SearchIcon />
+          </i>
+          <input
+            type="text"
+            class="SideRight-head-content-left-input"
+            placeholder="search..."
+          />
+        </div>
 
-      <div class="SideRight-head-right">
-        <i class="SideRight-head-right-icon">
-          <BellIcon />
-        </i>
-        <span class="SideRight-head-right-name">hi, olayinka</span>
-        <img :src="image" alt="" class="SideRight-head-right-name-img" />
+        <div class="SideRight-head-content-right">
+          <i class="SideRight-head-content-right-icon">
+            <BellIcon />
+          </i>
+          <span class="SideRight-head-content-right-name">hi, olayinka</span>
+          <img :src="image" alt="" class="SideRight-head-content-right-img" />
+        </div>
+      </div>
+      <div class="padding">
+        <div class="SideRight-head-out mobile">
+          <i class="SideRight-head-out-icon">
+            <SearchIcon />
+          </i>
+          <input
+            type="text"
+            class="SideRight-head-out-input"
+            placeholder="search..."
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { BellIcon, SearchIcon } from '@vue-icons/feather';
+import { BellIcon, SearchIcon, MenuIcon } from '@vue-icons/feather';
 import image from '@/assets/man.png';
 
 export default {
-  components: { BellIcon, SearchIcon },
+  components: { BellIcon, SearchIcon, MenuIcon },
   data() {
     return { image: image };
   }
