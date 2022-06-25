dff108fae4f6f1f6188a3ecdcb112df1f24e1710
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-06-25 06:51:30 +0100

    link

--- src/components/SideLeft.vue
+++ src/components/SideLeft.vue
@@ -3,27 +3,27 @@
     <div class="SideLeft-head">360 coporation</div>
     <div class="SideLeft-body">
       <div class="SideLeft-body-links">
-        <div class="SideLeft-body-links-link">
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><GridIcon /></i>
           <span class="SideLeft-body-links-link-name">dashboard</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">settings</span>
           <i class="SideLeft-body-links-link-right_icon"><ChevronRightIcon /></i>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><MessageSquareIcon /></i>
           <span class="SideLeft-body-links-link-name">feeds</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <Icon class="SideLeft-body-links-link-icon">
             <CloneRegular />
           </Icon>
           <span class="SideLeft-body-links-link-name">user managements</span>
           <i class="SideLeft-body-links-link-right_icon"><ChevronRightIcon /></i>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
             <Icon class="SideLeft-body-links-link-icon">
               <AddressCardRegular />
             </Icon>
@@ -31,40 +31,40 @@
             >contacts managements</span
           >
           <i class="SideLeft-body-links-link-right_icon"><ChevronRightIcon /></i>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">departments</span>
           <i class="SideLeft-body-links-link-right_icon"><ChevronRightIcon /></i>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">schedule & reports</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">inventory</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">sales & payments</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">companies</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">requests</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">messaging</span>
-        </div>
-        <div class="SideLeft-body-links-link">
+        </a>
+        <a href="#!" class="SideLeft-body-links-link">
           <i class="SideLeft-body-links-link-icon"><SettingsIcon /></i>
           <span class="SideLeft-body-links-link-name">conferencing</span>
-        </div>
+        </a>
       </div>
     </div>
   </div>
